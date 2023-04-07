3cf334345d0743aec292984e7454a5159b919260
hannesa2 <hannesa2@users.noreply.github.com>
2023-04-07 12:39:28 +0100

    Bump gitx 0.23-46-ge6a0348d

--- Casks/gitx.rb
+++ Casks/gitx.rb
@@ -1,9 +1,8 @@
 cask "gitx" do
   arch arm: "arm64", intel: "x86_64"
 
-  version "0.23"
-  sha256 arm:   "6b350adf5d38826d834a25e0cdacb3f3bc38670cbd928a59bd7982b1d528ea0f",
-         intel: "ba5edc82c260fbdf4b6e74c2e3a455b270ebde6d5c94a9140f212bec1cab9507"
+  version "0.23-46-ge6a0348d"
+  sha256 arm:   "863f740a3df2ddb2e4d9396a1e36da574e2e7521af7aeb3836875756d0b1f971",
 
   url "https://github.com/gitx/gitx/releases/download/#{version}/GitX-#{arch}.dmg"
   name "GitX"
